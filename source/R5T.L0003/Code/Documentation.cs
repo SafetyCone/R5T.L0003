89031d73f17ac7d0acf3f74861c31bfef49855b6
David Coats <david@getsafetycone.com>
2021-09-13 14:05:55 -0700

    Add documentation.

---
+++ source/R5T.L0003/Code/Documentation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.L0003
+{
+    /// <summary>
+    /// <see cref="Octokit"/> Nuget package selector.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
